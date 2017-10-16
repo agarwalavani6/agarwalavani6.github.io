b415575cef06947c517494373352dfd0aa570dbd
agarwalavani6 <32076497+agarwalavani6@users.noreply.github.com>
2017-10-16 12:56:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
     s("#wrapper").fadeIN(1500);
 });
+
+function thatWasEasy(){
+    alert("That was easy!")
+}
+$('#easy').on("click", thatWasEasy);
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event){
+    if(event.keyCode == 32) {
+        S("#easy").trigger("click");
+    }
+}
+ 
